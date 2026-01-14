be6889ae7a2f275a0a8924c4aa836b4f7e091bd8
amrithavr-13 <mishwallace13@gmail.com>
2026-01-14 10:48:00 +0530

    Implement basic login check using hardcoded values

---
+++ CW_3.java
@@ -0,0 +1,13 @@
+class LoginCheck {
+    public static void main(String[] args) {
+
+        String username = "admin";
+        String password = "java123";
+
+        if (username == "admin" && password == "java123") {
+            System.out.println("Login Successful");
+        } else {
+            System.out.println("Access Denied");
+        }
+    }
+}
